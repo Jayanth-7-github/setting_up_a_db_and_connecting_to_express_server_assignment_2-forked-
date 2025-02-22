419749c685862c38f8af0eb429b5b7eedaf0e314
Jayanth <kolleparajayanth@gmail.com>
2025-02-22 11:05:32 +0530

    done

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
-const express = require('express');
-const mongoose=require("mongoose")
-const { resolve } = require('path');
-// const User = mongoose.model('User', userSchema);
-const User =require("./schema")
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const { resolve } = require("path");
+const User = require("./schema");
+require("dotenv").config();
 const app = express();
 const port = 3010;
 
-const url=process.env.mongodb
-const connect=mongoose.connect(process.env.mongodb)
+const url = process.env.mongodb;
+const connect = mongoose.connect(process.env.mongodb);
 
-app.use(express.json());  
+app.use(express.json());
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
-// app.use("/api",userRoute)
-
-
-app.post("/api/users",(async(req,res)=>{
-  const{name,email,password}=req.body;
+app.post("/api/users", async (req, res) => {
+ 
   try {
-    if (name && email && password)
-{
-  const user=new User({name,email,password});
-  await user.save();
-  res.status(201).json({message:"User created successfully",user});
-}  
-else{
-  res.status(400).json({message:"invalid"});
-
-}
-} catch (error) {
-  res.status(500).json({message:"internal server error"}) 
-
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    if(error.name==="ValidationError"){
+      return res.status(400).json({message:error.message})
+    }
+    res.status(500).json({ message: "internal server error" });
   }
-}))
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
